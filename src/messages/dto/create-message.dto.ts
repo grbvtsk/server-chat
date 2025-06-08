01bd248b70e5fbd304f9a267479b299b8a6e8efb
grbvtsk <ngraboveckij@gmail.com>
2025-06-08 11:49:52 +0300

    update-message-dto

--- src/messages/dto/create-message.dto.ts
+++ src/messages/dto/create-message.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateMessageDto {
+  @IsString()
+  @IsNotEmpty()
   nickname: string;
+
+  @IsString()
+  @IsNotEmpty()
   content: string;
 }
