79b75d97092f2edfe3d21412cd340fc8aae45881
grbvtsk <ngraboveckij@gmail.com>
2025-06-08 12:12:13 +0300

    changed-cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://client-chat-x985.vercel.app'],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
